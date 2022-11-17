3b695ca3a705d5fe25d4917d398e282cedbc15b3
Milos Knezevic <ushtipak@gmail.com>
2022-11-17 15:25:32 +0100

    Add Py requirements and version support, C / Py / Go support for app name :)

--- assets/js/dockerup.js
+++ assets/js/dockerup.js
@@ -1,6 +1,11 @@
+function hideUnused() {
+  document.getElementById("hidden-py").setAttribute("hidden", "");
+}
+
 function dockerup() {
   var dockerfileText = document.getElementById('dockerfile');
   var language = document.getElementById('language').value;
+  var app = document.getElementById('app').value;
   var env = document.getElementById('env').value;
   var ports = document.getElementById('ports').value;
 
@@ -23,53 +28,77 @@ function dockerup() {
     });
   };
 
-  var pythonBuildSpec = 'FROM python:3.10-alpine\n'                             +
-                        'WORKDIR /app\n'                                        +
-                        'RUN apk add build-base\n'                              +
-                        'COPY . .\n'                                            +
-                        'ENV VIRTUAL_ENV=/venv\n'                               +
-                        'RUN python3.10 -m venv $VIRTUAL_ENV\n'                 +
-                        'ENV PATH="$VIRTUAL_ENV/bin:$PATH"\n'                   +
-                        'RUN pip3 install --upgrade pip\n'                      +
-                        'RUN pip3 install --no-cache-dir wheel\n'               +
-                        'RUN pip3 install --no-cache-dir -r requirements.txt\n' +
-                        'ENV PYTHONUNBUFFERED=1';
-  var pythonRunSpec = 'CMD ["python3", "app.py"]'
-
-  var goBuildSpec = 'FROM golang:alpine AS builder\n' +
-                    'WORKDIR /app\n'                  +
-                    'RUN apk add build-base\n'        +
-                    'COPY . .\n'                      +
-                    'RUN go build -o app main.go'
-  var goRunSpec = 'FROM alpine:latest\n'         +
-                  'COPY --from=builder /app .\n' +
-                  'CMD ["./app"]'
-
-  var cFile = 'test.c';
-  var cBuildSpec = 'FROM gcc:latest AS builder\n' +
-                   'WORKDIR /build\n'             +
-                   'COPY . .\n'                   +
-                   `RUN gcc -o app ${cFile}`
-  var cRunSpec = 'FROM alpine:latest\n'               +
-                 'WORKDIR /app\n'                     +
-                 'RUN apk add libc6-compat\n'         +
-                 'COPY --from=builder /build/app .\n' +
-                 'ENTRYPOINT ["./app"]'
-
+  // if language is chosen, pre-hide specific, non-selected options
   if (language != '') {
+    hideUnused();
+
     switch(language) {
       case 'python':
-        buildsAndDependencies = pythonBuildSpec;
+        document.getElementById("hidden-py").removeAttribute("hidden");
+        document.getElementById('app').placeholder = "e.g. app.py";
+        var pyVersion = document.getElementById('py-version').value;
+        var pyRequirements = document.getElementById('py-requirements').value;
+        if (app == '') {
+          app = 'app.py'
+        }
+
+        var pythonBuildSpec = `FROM python:${pyVersion}-alpine\n`                             +
+                              'WORKDIR /app\n'                                        +
+                              'RUN apk add build-base\n'                              +
+                              'COPY . .\n'                                            +
+                              'ENV PYTHONUNBUFFERED=1';
+        var pythonBuildVenv = 'ENV VIRTUAL_ENV=/venv\n'                               +
+                              'RUN python3.10 -m venv $VIRTUAL_ENV\n'                 +
+                              'ENV PATH="$VIRTUAL_ENV/bin:$PATH"\n'                   +
+                              'RUN pip3 install --upgrade pip\n'                      +
+                              'RUN pip3 install --no-cache-dir wheel\n'               +
+                              'RUN pip3 install --no-cache-dir -r requirements.txt'
+        var pythonRunSpec = `CMD ["python3", "${app}"]`
+
+
+        pyRequirements != '' ? buildsAndDependencies = pythonBuildSpec + '\n' + pythonBuildVenv : buildsAndDependencies = pythonBuildSpec
         envPortsAndRun = envAndPorts + pythonRunSpec;
         break;
+
       case 'go':
+        if (app == '') {
+          app = 'main.go'
+        }
+
+        var goBuildSpec = 'FROM golang:alpine AS builder\n' +
+                          'WORKDIR /build\n'                  +
+                          'RUN apk add build-base\n'        +
+                          'COPY . .\n'                      +
+                          `RUN go build -o app ${app}`
+        var goRunSpec = 'FROM alpine:latest\n'         +
+                        'COPY --from=builder /build .\n' +
+                        'CMD ["./app"]'
+
         buildsAndDependencies = goBuildSpec;
         envPortsAndRun = envAndPorts + goRunSpec;
+        document.getElementById('app').placeholder = "e.g. cmd/app-name/main.go";
         break;
+
       case 'c':
+        if (app == '') {
+          app = 'main.c'
+        }
+
+        var cBuildSpec = 'FROM gcc:latest AS builder\n' +
+                         'WORKDIR /build\n'             +
+                         'COPY . .\n'                   +
+                         `RUN gcc -o app ${app}`
+        var cRunSpec = 'FROM alpine:latest\n'               +
+                       'WORKDIR /app\n'                     +
+                       'RUN apk add libc6-compat\n'         +
+                       'COPY --from=builder /build/app .\n' +
+                       'ENTRYPOINT ["./app"]'
+
         buildsAndDependencies = cBuildSpec;
         envPortsAndRun = envAndPorts + cRunSpec;
+        document.getElementById('app').placeholder = "e.g. main.c";
         break;
+
       default:
     }
 
